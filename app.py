b034c6eccf989c8ee3f3763bea056558c2ec1424
kristy chao <kristy334420@gmail.com>
2022-03-27 19:12:15 +0800

    week4

--- app.py
+++ app.py
@@ -257,7 +257,7 @@ def page_not_found(error):
 
 import jwt
 
-# 使用者API
+# 使用者登入系統API
 # 新增member表單
 # mycursor.execute("CREATE TABLE member (id bigint PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL,email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP)")
 # mydb.commit()
