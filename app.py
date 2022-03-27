b55f42dc2c75fa44625d70b9e102642431b59196
kristy chao <kristy334420@gmail.com>
2022-03-27 19:17:01 +0800

    week4 ver1

--- app.py
+++ app.py
@@ -33,40 +33,40 @@ def thankyou():
 #=================================== 雲端資料庫連線==========================
 
 
-# import mysql.connector
-# import boto3
-# import json
+import mysql.connector
+import boto3
+import json
 
-# client=boto3.client('secretsmanager')
-# response=client.get_secret_value(SecretId='conSQL')
+client=boto3.client('secretsmanager')
+response=client.get_secret_value(SecretId='conSQL')
 
-# secretDict=json.loads(response['SecretString'])
-# mydb = mysql.connector.connect(
-#     host=secretDict['host'],
-#     user=secretDict['username'],
-#     password=secretDict['password'],
-#     database="travelsite"
-# )
+secretDict=json.loads(response['SecretString'])
+mydb = mysql.connector.connect(
+    host=secretDict['host'],
+    user=secretDict['username'],
+    password=secretDict['password'],
+    database="travelsite"
+)
 
-# print(mydb)
-# mycursor=mydb.cursor()
+print(mydb)
+mycursor=mydb.cursor()
 
 #========================================本機資料庫連線=========================================
 
-import mysql.connector
-import os
+# import mysql.connector
+# import os
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    # user=os.environ['DB_USER'],
-    # password=os.environ['DB_PWD'],
-    user=os.environ.get('DB_USER'),
-    password=os.environ.get('DB_PWD'),
-    database="travelsite",
-	port="3306"
-)
-print(mydb)
-mycursor=mydb.cursor()
+# mydb = mysql.connector.connect(
+#     host="localhost",
+#     # user=os.environ['DB_USER'],
+#     # password=os.environ['DB_PWD'],
+#     user=os.environ.get('DB_USER'),
+#     password=os.environ.get('DB_PWD'),
+#     database="travelsite",
+# 	port="3306"
+# )
+# print(mydb)
+# mycursor=mydb.cursor()
 
 #=========================================本機版結束=============================
 # Attraction APIs
